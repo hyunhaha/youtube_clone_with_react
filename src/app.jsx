aedc52f8837434305a9b8e8811ab09af64cd306c
hyunhaha <gusgk2658@naver.com>
2020-10-16 02:27:29 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./app.css";
+import VideoList from "./components/video_list";
 
 function App() {
-  return <h1>hello </h1>;
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCxSlgloQ2rfNxXGFqTMh6DdqS3NWqcBXc",
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log("error", error));
+  }, [videos]);
+
+  return <VideoList key={videos.id} videos={videos} />;
 }
 
 export default App;
